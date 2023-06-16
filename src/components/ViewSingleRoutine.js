c2794e58ffbbf95e26b5215448e15821cd9f6bdc
Kevin Harp <kharp1984@gmail.com>
2023-06-16 17:34:24 -0400

    updated getSingleRoutine references away from "posts"

--- src/components/ViewSingleRoutine.js
+++ src/components/ViewSingleRoutine.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { deleteRoutine } from "../ajax-requests";
 
 const ViewSingleRoutine = ({ currentRoutine, token, setCurrentRoutine, setToken }) => {
-
+  
   async function handleDelete(routineId, token) {
     const results = await deleteRoutine(routineId, token);
     if (results.success) {
@@ -24,18 +24,18 @@ const ViewSingleRoutine = ({ currentRoutine, token, setCurrentRoutine, setToken 
              <Link to="/">Back to Listings</Link>
            </React.Fragment>
           :<React.Fragment>
-             <Link to="/createPost">Create Listing</Link>
-             <Link to="/profile">Profile</Link>
-             <Link to="/">Back to Listings</Link>
+             <Link to="/createRoutine">Create Routine</Link>
+             <Link to="/myRoutines">My Routines</Link>
+             <Link to="/">Back to Routines</Link>
              <Link to="/" onClick={logout}>Logout</Link>
            </React.Fragment>
         }
       </nav>
       <div className="post">
-        <h2>{currentPost.title}</h2>
-        <p>{currentPost.description}</p>
-        <h4>{currentPost.price}</h4>
-        <h4>{currentPost.location}</h4>
+        <h2>{currentRoutine.title}</h2>
+        <p>{currentRoutine.description}</p>
+        <h4>{currentRoutine.price}</h4>
+        <h4>{currentRoutine.location}</h4>
         <div>
           { currentPost.isAuthor ? <Link to="/updatePost"><button onClick={() => {setCurrentPost(currentPost)}}>UPDATE</button></Link> : null }
           { currentPost.isAuthor ? <button onClick={() => handleDelete(currentPost._id, token)}>DELETE</button> : null }
